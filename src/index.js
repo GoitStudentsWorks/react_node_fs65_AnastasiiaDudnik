0599ff6f82e0d0f3d8641091354f45a09abc0b3e
Zub04ek <nastasyz@ukr.net>
2023-06-24 22:29:26 +0300

    added theme provider

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import { store, persistor } from 'redux/store';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from 'theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goose-track-frontend">
-          <App />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
