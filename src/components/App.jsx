1f8e6c3895530cd1c981326b68971e6b672e7df8
volodymyr-kyrylenko-c0x12c <134924457+volodymyr-kyrylenko-c0x12c@users.noreply.github.com>
2023-06-26 16:14:39 +0300

    Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,27 @@
-import { Suspense, lazy } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
+import { Suspense, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import { MainTemplate } from 'template/MainTemplate';
 
-
-const LoginPage = lazy(() => import("pages/loginPage/loginPage"))
-const RegisterPage = lazy(() => import("pages/registerPage/registerPage"))
-
-
+const LoginPage = lazy(() => import('pages/loginPage/loginPage'));
+const RegisterPage = lazy(() => import('pages/registerPage/registerPage'));
 
 export const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <Suspense fallback={null}>
       {/* Заміть null має бути лоадер */}
-    <Routes>
-      <Route path='/' element={<PrivateRoute />}>
-        <Route index element={<div>Main Page</div>} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<MainTemplate />}>
+            <Route index element={<div>Main Page</div>} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="login" element={<LoginPage />} />
+          </Route>
+        </Route>
+      </Routes>
     </Suspense>
   );
 };
---
+++ src/icons/Icons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import IconsSVG from './sprite.svg';
+
+function Icons({ name, color, size, className }) {
+  return (
+    <svg
+      className={`icon icon-${name} ${className}`}
+      fill={color}
+      stroke={color}
+      width={size}
+      height={size}
+    >
+      <use xlinkHref={`${IconsSVG}#${name}`} />
+    </svg>
+  );
+}
+
+export default Icons;
