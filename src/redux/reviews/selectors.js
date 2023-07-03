248434cc0d2f24de6b48154777a7765d5f19a05b
Zub04ek <nastasyz@ukr.net>
2023-07-03 14:27:03 +0300

    add redux to work with user reviews

--- src/redux/reviews/selectors.js
+++ src/redux/reviews/selectors.js
@@ -1,5 +1,7 @@
 export const selectReviews = state => state.reviews.reviews;
 
+export const selectUserReview = state => state.reviews.userReview;
+
 export const selectIsRefreshing = state => state.reviews.isRefreshing;
 
 export const selectError = state => state.reviews.error;
