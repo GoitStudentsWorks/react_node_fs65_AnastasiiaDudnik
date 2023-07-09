349f8bd4a675a2c8d7ac077cb215dfc4e1dc24e7
Kindual <110603179+Kindual@users.noreply.github.com>
2023-07-09 16:11:53 +0300

    Update choosedDay.jsx

--- src/components/choosedDay/choosedDay.jsx
+++ src/components/choosedDay/choosedDay.jsx
@@ -4,137 +4,118 @@ import DayCalendarHead from 'components/dayCalendarHead/dayCalendarHead';
 import dayjs from 'dayjs';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useLocation, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getWeekTasks } from 'redux/tasks/operations';
 
-const ChoosedDay = ({ date }) => {
+export default function ChoosedDay() {
+  const params = useParams()
+  const [day, setDay] = useState(() => params.day);
+  const [value, setValue] = React.useState(null);
   const dispatch = useDispatch();
-  const location = useLocation();
-  const [weekStartDate, setWeekStartDate] = useState(
-    dayjs(location.pathname.slice(19, 29))
-  );
-  const [value, setValue] = useState(null);
-  const [prevDate, setPrevDate] = useState('');
-  const { day } = useParams();
+  const navigate = useNavigate();
+
+  const [weekYear, setWeekYear] = useState(null);
 
   useEffect(() => {
-    const newStartDate = dayjs(location.pathname.slice(19, 29));
-    setWeekStartDate(newStartDate);
-  }, [location.pathname]);
+    if (day !== params.day) {
+      setDay(params.day)
+    }
 
-  useEffect(() => {
-    setPrevDate(day);
-  }, [day]);
-
-  useEffect(() => {
-    setPrevDate(location.pathname.slice(27, 29));
-  }, [location.pathname]);
+  }, [day, params.day])
 
   const weekend = useMemo(() => {
     const arr = [];
     let i = 0;
     let y = 7;
 
-    if (weekStartDate.weekday() === 0) {
+    if (dayjs(day).weekday() === 0) {
       i = -7;
       y = 0;
     }
+    let e = 0;
+    for (i; i < y; i++) {
+      const test = dayjs(day).weekday(i + 1);
 
-    for (let j = i; j < y; j++) {
-      const test = weekStartDate.weekday(j + 1);
-
-      arr.push({
+      arr[e] = {
         day: test.$D,
         weekDay: String(test.$d).slice(0, 3),
         weekDayMob: String(test.$d).slice(0, 1),
         date: test.$d,
-      });
+        dayFormat: dayjs(test.$d).format('YYYY-MM-DD')
+      };
+      e++;
     }
-
     return arr;
-  }, [weekStartDate]);
+  }, [day]);
 
-  const addZero = num => {
-    return num < 10 ? `0${num}` : num;
-  };
+  function addZero(num) {
+    if (num < 10) {
+      return `0${num}`;
+    }
+    return num;
+  }
+
+  useEffect(() => {
+    if (weekend) {
+      const index = weekend.findIndex(({ dayFormat }) =>
+        dayFormat === day
+      );
+
+      setValue(index)
+    }
+  }, [day, weekend])
 
   useEffect(() => {
     if (value === null) {
-      setValue(weekStartDate.day() === 0 ? 6 : weekStartDate.day() - 1);
-    } else {
-      const currentDate = location.pathname.slice(27, 29);
-
-      if (currentDate > prevDate) {
-        setValue(prevValue => {
-          if (prevValue === 6) {
-            setWeekStartDate(prevStartDate => prevStartDate.add(1, 'week'));
-            return 0;
-          }
-          return prevValue + 1;
-        });
-      } else if (currentDate < prevDate) {
-        setValue(prevValue => {
-          if (prevValue === 0) {
-            setWeekStartDate(prevStartDate =>
-              prevStartDate.subtract(1, 'week')
-            );
-            return 6;
-          }
-          return prevValue - 1;
-        });
-      }
-
-      setPrevDate(currentDate);
+      setValue(dayjs(day).day() === 0 ? 6 : dayjs(day).day() - 1);
     }
-  }, [date, location.pathname, prevDate, weekStartDate, value]);
+  }, [day, value]);
 
   useEffect(() => {
-    if (weekend[0]) {
-      const { date } = weekend[0];
-      const years = new Date(date).getFullYear();
-      const month = addZero(new Date(date).getMonth() + 1);
-      const day = addZero(new Date(date).getDate());
+    const currentWeek = dayjs(weekend[0].dayFormat).week(); 
 
-      dispatch(getWeekTasks({ years, month, day }));
+    if (weekend[0] && currentWeek !== weekYear) {
+      dispatch(
+        getWeekTasks({
+          years: new Date(weekend[0].date).getFullYear(),
+          month: addZero(new Date(weekend[0].date).getMonth() + 1),
+          day: addZero(new Date(weekend[0].date).getDate()),
+        })
+      );
+      setWeekYear(currentWeek)
     }
-  }, [weekStartDate, dispatch, weekend]);
+  }, [dispatch, weekYear, weekend]);
 
   const handleChange = (e, newValue) => {
     setValue(newValue);
+    navigate(`/main/calendar/day/${weekend[newValue].dayFormat}`)
   };
 
-  if (!weekStartDate || value === null || !weekend) {
-    return null;
-  }
+
+  if (!day) { return }
+  if (value === null || !weekend) { return }
 
   return (
     <Box sx={style.boxDay}>
-      <Box sx={{ boxSizing: 'border-box', borderColor: 'divider' }}>
-        <DayCalendarHead
-          value={value}
-          weekend={weekend}
-          handleChange={handleChange}
-        />
+      <Box sx={{ boxSizing: 'border-box' }}>
+        <Box sx={{ borderColor: 'divider' }}>
+          <DayCalendarHead
+            value={value}
+            weekend={weekend}
+            handleChange={handleChange}
+          />
+        </Box>
       </Box>
       <ColumnsTasksList weekend={weekend} value={value} />
     </Box>
   );
-};
+}
 
 const style = {
-  '&::-webkit--scrollbar': {
-    borderRadius: '5px',
-    width: '.65em',
-    backgroundColor: '#ffffff',
-  },
-  '&::-webkit-scrollbar-track, &::-webkit-scrollbar-corner': {
-    backgroundColor: 'rgba(170, 170, 170, 0)',
-  },
   '&::-webkit-scrollbar-thumb, &::-webkit-resizer': {
     background: 'rgba(170, 170, 170, 0.6)',
     borderRadius: '5px',
-    boxShadow:
-      'inset 0.05em 0.05em 0 rgba(0, 0, 0, 0.1), inset 0 - 0.05em 0 rgba(0, 0, 0, 0.07)',
+    boxShadow: 'inset 0.05em 0.05em 0 rgba(0, 0, 0, 0.1), inset 0 - 0.05em 0 rgba(0, 0, 0, 0.07)',
   },
   boxDay: {
     width: {
@@ -145,5 +126,3 @@ const style = {
     margin: '0',
   },
 };
-
-export default ChoosedDay;
