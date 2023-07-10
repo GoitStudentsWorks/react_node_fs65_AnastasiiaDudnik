5e332440b3d2c5fcda1eadc6085bf62e01a5ef33
VovaKyrylenko <110337169+VovaKyrylenko@users.noreply.github.com>
2023-07-10 09:44:22 +0300

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -18,7 +18,6 @@ import { getUserReview } from '../../redux/reviews/operations';
 import { selectUser } from '../../redux/auth/selectors';
 import { useLocation } from 'react-router-dom';
 import hay from './hay.png';
-import { selectTasks } from 'redux/tasks/selectors';
 
 export const Header = ({
   handleDrawerToggle,
